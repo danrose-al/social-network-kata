a982f770872556d01c30a11cae4806f5248ef278
Roisin Oakey <roisin.oakey@artlogic.net>
2024-11-26 16:53:44 +0000

    rename param for readability"

--- social_network/parse_input.py
+++ social_network/parse_input.py
@@ -4,20 +4,20 @@ class ParseInput:
     def __init__(self, social_network_api = None):
         self.social_network_api = social_network_api or SocialNetworkAPI() 
 
-    def process(self, input: str) -> None:
-        if "->" in input:
-            split_list = [data.strip() for data in input.split("->")]
+    def process(self, user_input: str) -> None:
+        if "->" in user_input:
+            split_list = [data.strip() for data in user_input.split("->")]
             user, message = split_list[0], split_list[1]
             self.social_network_api.post(user, message)
-        elif len(input.split(' ')) == 1:
-            user = input
+        elif len(user_input.split(' ')) == 1:
+            user = user_input
             self.social_network_api.read(user)
-        elif "follows" in input:
-            split_list = [data.strip() for data in input.split("follows")]
+        elif "follows" in user_input:
+            split_list = [data.strip() for data in user_input.split("follows")]
             follower, followee = split_list[0], split_list[1]
             self.social_network_api.follows(follower, followee)
-        elif "wall" in input:
-            user = input.replace("wall", "").strip()
+        elif "wall" in user_input:
+            user = user_input.replace("wall", "").strip()
             self.social_network_api.wall(user)
 
 
